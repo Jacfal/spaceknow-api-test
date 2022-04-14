08adca7e36ad2c089d688df601604821fd6fb05b
Jakub Pravda <jakub.pravda@comsource.cz>
2022-04-14 22:49:50 +0200

    retreive scene id function

--- src/spaceknow_api/apis/ragnar_task.py
+++ src/spaceknow_api/apis/ragnar_task.py
@@ -1,4 +1,5 @@
-from apis.async_api import AsyncTask
+from array import array
+from apis.async_task import AsyncTask
 from apis.auth_api import AuthApi
 from apis.task_api import TaskApi
 
@@ -6,3 +7,7 @@ from apis.task_api import TaskApi
 class RagnarTask(AsyncTask):
   def __init__(self, client_api: AuthApi, task_api: TaskApi) -> None:
       super().__init__('https://api.spaceknow.com/imagery', client_api, task_api)
+
+  def retreive_scene_ids(self) -> set:
+    retreive_result = super().retreive()
+    return set(filter(lambda i: i, map(lambda j: j.get('sceneId', None), retreive_result)))   
